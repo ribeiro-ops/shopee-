aab18915ca0ddb6b048791b9ce35584a4a4b55b1
Leonardo ribeiro <leorib12433@gmail.com>
2026-05-04 12:05:07 -0300

    Enhance cart and wishlist functionality
    Refactor cart and wishlist display logic with comments.

--- commonjs-modules/src/index.js
+++ commonjs-modules/src/index.js
@@ -1,18 +1,29 @@
-import * as cartService from "./services/cart.js"
+import * as cartService from "./services/cart.js";
 import createItem from "./services/item.js";
 
 const myCart = [];
 const myWhishList = [];
 
-console.log("Welcome to the your Shopee Cart!");
+console.log("Welcome to your Shopee Cart!");
 
+// criando itens
 const item1 = await createItem("hotwheels ferrari", 20.99, 7);
 const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
 
+// adicionando ao carrinho
 await cartService.addItem(myCart, item1);
+
+// adicionando na wishlist
 await cartService.addItem(myWhishList, item2);
 
-console.log("Shopee Cart TOTAL IS!");
-await cartService.calcularTotal(myCart)
+// mostrando carrinho
+console.log("\n🛒 CART LIST:");
+await cartService.displayCart(myCart);
 
+// mostrando wishlist
+console.log("\n💖 WISHLIST:");
+await cartService.displayCart(myWhishList);
 
+// total
+console.log("\n💰 Shopee Cart TOTAL IS:");
+await cartService.calcularTotal(myCart);
